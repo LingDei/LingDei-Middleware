16d14602f3f7e6484cb0626ab7286419f8ae3fee
zjy2414 <zjy2414@outlook.com>
2023-10-27 01:00:53 +0800

    Revert "update: 建立基础模型"
    This reverts commit 5375200e88fcf723f43c3f8c7e52cb1664b9987e.

--- model/user.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type User struct {
-	ID       string `json:"id" validate:"required" gorm:"primaryKey"`
-	UserName string `json:"username" validate:"required,min=3,max=20" gorm:"not null; unique"`
-	Password string `json:"password" validate:"required,min=6,max=20" gorm:"not null"`
-	Role     int    `json:"role" gorm:"not null; default:0"`
-}

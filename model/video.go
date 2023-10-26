d057175da02080b3f7b8b5e8fac24c96e8c4a425
zjy2414 <zjy2414@outlook.com>
2023-10-27 02:13:10 +0800

    update: video model

--- model/video.go
+++ model/video.go
@@ -6,7 +6,7 @@ type Video struct {
 	Category      string `json:"category" validate:"required"`
 	Thumbnail_URL string `json:"thumbnail_url" validate:"required"`
 	Views         int    `json:"views" validate:"required"`
-	Author_ID     string `json:"author_id" validate:"required"`
+	Author_UUID   string `json:"author_id" validate:"required"`
 }
 
 type VideoResp struct {

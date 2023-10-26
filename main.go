e8f069ac2d77ea74ea4f255a2b84a9b760c60841
zjy2414 <zjy2414@outlook.com>
2023-10-27 00:53:46 +0800

    update: 初始化fiber应用

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+//	@title						灵嘚中间件
+//	@version					v1.0.0
+//	@description				灵嘚中间件（LingDei-Middleware）
+//	@host						127.0.0.1:9000
+//	@BasePath					/
+//	@securityDefinitions.apikey	ApiKeyAuth
+//	@in							header
+//	@name						Authorization
+
+func main() {
+	// 创建 Fiber 实例
+	app := fiber.New()
+
+	// 启动 CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowCredentials: true,
+	}))
+
+	// 注册服务
+	// regiserService(app)
+
+	// 启动服务
+	app.Listen(":9000")
+}

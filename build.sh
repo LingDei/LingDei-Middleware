56e3131cdf68b6a37438929280435514615c3833
zjy2414 <zjy2414@outlook.com>
2023-10-28 18:05:24 +0800

    update

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
-local_url=127.0.0.1:9000
-url=api.lingdei.doyi.online
+local_url="127.0.0.1:9000"
+url="api.lingdei.doyi.online"
 
-mkdir -p ./build
+rm -rf ./build
 
-rm -rf ./build/*
-cp -r * ./build
+mkdir ./build
+
+# copy 除了build目录以外的所有文件
+cp -r `ls | grep -v build` ./build
 
 cd ./build
 
-sed -i "s/'${local_url}'/'${url}'/g" main.go
+sed -i "s/${local_url}/${url}/g" main.go
 swag fmt
 swag init
 

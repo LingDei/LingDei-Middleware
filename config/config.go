97ac99766f5ec71d51fe42bd10bcbb8280f0b98b
zjy2414 <zjy2414@outlook.com>
2023-10-29 01:54:30 +0800

    update: 增加db_name

--- config/config.go
+++ config/config.go
@@ -18,6 +18,7 @@ var (
 	//私钥路径
 	PrivateKeyPath = "./config/rsa_private_key.pem"
 
+	DB_NAME = "LingDei"
 	DSN = "lingdei:lingdei123456@tcp(127.0.0.1:3306)/LingDei?charset=utf8mb4&parseTime=True&loc=Local"
 
 	// QiNiu

d4f10790ccb3c9dd587932a64bdf894f4cce33e3
zjy2414 <zjy2414@outlook.com>
2023-10-29 12:46:40 +0800

    update: 指定类型为varchar

--- model/collect.go
+++ model/collect.go
@@ -3,8 +3,8 @@ package model
 // Collect 收藏
 type Collect struct {
 	UUID       string `json:"uuid" validate:"required" gorm:"primaryKey"`
-	Video_UUID string `json:"video_uuid" validate:"required"`
-	User_UUID  string `json:"user_uuid" validate:"required"`
+	Video_UUID string `json:"video_uuid" validate:"required" gorm:"type:varchar(191)"`
+	User_UUID  string `json:"user_uuid" validate:"required" gorm:"type:varchar(191)"`
 	Video      Video  `json:"video,omitempty" validate:"-" gorm:"-"`
 }
 

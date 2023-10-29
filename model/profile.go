e08fb8b73dabf970635d3b44f80ae32b87ccf4f9
zjy2414 <zjy2414@outlook.com>
2023-10-29 23:55:38 +0800

    fix

--- model/profile.go
+++ model/profile.go
@@ -3,7 +3,7 @@ package model
 type Profile struct {
 	ID        string `json:"id" validate:"required" gorm:"primaryKey"`
 	NickName  string `json:"nickname" validate:"required,min=2,max=25" gorm:"not null, default:用户名称"`
-	AvatarURL string `json:"avatarurl"`
+	AvatarURL string `json:"avatar_url"`
 	Email     string `json:"email"`
 }
 
